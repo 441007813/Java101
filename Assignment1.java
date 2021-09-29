691b0cbc252452230917a5a452e9b7ae96d975a5
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 11:27:10 +0300

    صلح المشكلة و تزيد الدرجة اللي ناقصة

--- Assignment1.java
+++ Assignment1.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
-
+// your grade 4/5
 //mnsor najaa
 //441007813
 class Main {
   public static void main(String[] args) {
-      //Start your code 
+      //Start your code
       Scanner myscanner = new Scanner(System.in);
+      //@ ask the user to enter a number
       float var1 = myscanner.nextFloat();
+      //@ ask the user to enter a number
       float var2 = myscanner.nextFloat();
       System.out.println("addition");
+      //@ use println
       System.out.print(var1+var2);
       System.out.println("subtraction");
       System.out.println(var1 - var2);
