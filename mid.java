d3d1f731d4b7f7c761f5842c2cb9ff13a5ef5ff7
منصور ناجع حسن المتعاني <91257392+441007813@users.noreply.github.com>
2021-10-19 10:59:25 +0300

    Create mid.java

---
+++ mid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Mid {
+	public static void main(String[] args) {
+	Scanner scen = new Scanner(System.in);
+	float var ;
+	System.out.println("enter a grade from 0 to 100");
+	var =scen.nextFloat();
+	if (var >= 90 && var <= 100){
+	   System.out.println(+ var +  "A");
+	}else if (var >= 80 ){
+	    System.out.format("Good job with" + var +  "out of 100" );
+	}else if (var >= 60){
+	    System.out.format("passed the course with" + var + "out of 100");
+	}else {
+	    System.out.println("sorry you fail or yuo entered a wrong grade");
+	}
+
+	}
+}
