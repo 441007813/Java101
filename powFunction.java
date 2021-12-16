1f2b197ec6bfe15fb53607c361ef7313d29e30ff
منصور ناجع حسن المتعاني <91257392+441007813@users.noreply.github.com>
2021-12-16 05:52:27 +0300

    Create powFunction.java

---
+++ powFunction.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner scan = new Scanner(System.in);
+	  System.out.println("enter a mumber");
+	  int var = scan.nextInt();
+	  System.out.println(var * var);
+	}
+}
